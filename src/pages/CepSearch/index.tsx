ab4be9d58ca3ddf5d9d767ed7d460755ed0080ae
João Paulo <jp_cbc@hotmail.com>
2024-01-04 11:21:05 -0300

    Formulário viacep

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,19 +1,70 @@
+import { useState } from 'react';
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import axios from 'axios';
+import InputMask from "react-input-mask";
+
+type FormData = {
+  cep: string;
+}
+
+type Address = {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ddd: string;
+}
 
 const CepSearch = () => {
+
+  const [address, setAddress] = useState<Address>();
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios.get(`https://viacep.com.br/ws/${formData.cep}/json/`)
+      .then((response) => {
+        setAddress(response.data);
+        setFormData({
+          cep: ''
+        })
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+        console.log(error);
+      });
+  }
+
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
-            <input
+            <InputMask
+              mask="99999-999"
               type="text"
+              name='cep'
+              value={formData.cep}
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
@@ -21,8 +72,17 @@ const CepSearch = () => {
           </div>
         </form>
 
-        <ResultCard title="Logradouro" description="Lalala" />
-        <ResultCard title="Número" description="234" />
+        {address &&
+          <>
+            <ResultCard title="CEP" description={address.cep} />
+            <ResultCard title="Logradouro" description={address.logradouro} />
+            <ResultCard title="Complemento" description={address.complemento} />
+            <ResultCard title="Bairro" description={address.bairro} />
+            <ResultCard title="Localidade" description={address.localidade} />
+            <ResultCard title="Estado" description={address.uf} />
+            <ResultCard title="Código de área" description={address.ddd} />
+          </>
+        }
 
       </div>
     </div>
